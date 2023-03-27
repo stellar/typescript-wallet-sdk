3e46c9e8e1080fbdde2963a5a4e9d1d4d7ade28c
Alec Charbonneau <aleccharb21@gmail.com>
2023-03-27 22:55:18 +0800

    comment

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as walletSdk from "./walletSdk";
 import { Keypair } from "stellar-sdk";
 
+// TODO - figure out why Keypair used in parent codebase throws error
 export { walletSdk, Keypair };
 export default { walletSdk };
