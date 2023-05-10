47f8c3f27182d3818a082b025100481b2416aaa5
Alec Charbonneau <aleccharb21@gmail.com>
2023-05-10 13:05:23 -0500

    small fixes found while testing (#4)
    * small fixes found whle testing
    
    * comment

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import * as walletSdk from "./walletSdk";
+import { Keypair } from "stellar-sdk";
 
-export { walletSdk };
+// TODO - figure out why Keypair used in parent codebase throws error
+export { walletSdk, Keypair };
 export default { walletSdk };
