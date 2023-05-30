f71659eae3cc90745e23afbeed814f9559f39eb4
Cássio Marcos Goulart <3228151+CassioMG@users.noreply.github.com>
2023-05-30 11:44:01 -0300

    Add getTransactionBy function (#10)

--- src/walletSdk/Anchor/index.ts
+++ src/walletSdk/Anchor/index.ts
@@ -1,16 +1,40 @@
 import { StellarTomlResolver } from "stellar-sdk";
 import axios from "axios";
+import queryString from "query-string";
 
 import { Auth } from "../Auth";
 import { Interactive } from "../interactive";
 import { TomlInfo, parseToml } from "../toml";
-import { ServerRequestFailedError } from "../exception";
+import { 
+  MissingTransactionIdError, 
+  ServerRequestFailedError, 
+  InvalidTransactionResponseError
+} from "../exception";
+
+function _normalizeTransaction(transaction) {
+  // some anchors return _id instead of id, so rewrite that
+  if (transaction._id && transaction.id === undefined) {
+    transaction.id = transaction._id;
+  }
+
+  // others provide amount but not amount_in / amount_out
+  if (
+    transaction.amount &&
+    transaction.amount_in === undefined &&
+    transaction.amount_out === undefined
+  ) {
+    transaction.amount_in = transaction.amount;
+    transaction.amount_out = transaction.amount;
+  }
+  return transaction;
+}
 
 // Do not create this object directly, use the Wallet class.
 export class Anchor {
   private homeDomain = "";
   private httpClient = null;
   private cfg;
+  private toml: TomlInfo;
 
   constructor(cfg, homeDomain: string, httpClient) {
     this.homeDomain = homeDomain;
@@ -18,14 +42,22 @@ export class Anchor {
     this.cfg = cfg;
   }
 
-  async getInfo(): Promise<TomlInfo> {
-    const toml = await StellarTomlResolver.resolve(this.homeDomain);
-    return parseToml(toml);
+  async getInfo(shouldRefresh?: boolean): Promise<TomlInfo> {
+    // return cached TOML values by default
+    if(this.toml && !shouldRefresh) {
+      return this.toml;
+    }
+
+    // fetch fresh TOML values from Anchor domain
+    const stellarToml = await StellarTomlResolver.resolve(this.homeDomain);
+    const parsedToml = parseToml(stellarToml);
+    this.toml = parsedToml;
+    return parsedToml;
   }
 
   async auth() {
-    const tomlInfo = await this.getInfo();
-    return new Auth(tomlInfo.webAuthEndpoint);
+    const toml = await this.getInfo();
+    return new Auth(toml.webAuthEndpoint);
   }
 
   interactive() {
@@ -44,6 +76,73 @@ export class Anchor {
         },
       });
       return resp.data;
+    } catch (e) {
+      throw new ServerRequestFailedError(e);
+    }
+  }
+
+  /**
+  * Get single transaction's current status and details. One of the [id], [stellarTransactionId],
+  * [externalTransactionId] must be provided.
+  *
+  * @param authToken auth token of the account authenticated with the anchor 
+  * @param id transaction ID
+  * @param stellarTransactionId stellar transaction ID
+  * @param externalTransactionId external transaction ID
+  * @return transaction object
+  * @throws [MissingTransactionIdError] if none of the id params is provided
+  * @throws [InvalidTransactionResponseError] if Anchor returns an invalid transaction
+  * @throws [ServerRequestFailedError] if server request fails
+  */
+  async getTransactionBy({
+    authToken,
+    id,
+    stellarTransactionId,
+    externalTransactionId,
+    lang,
+  }: {
+    authToken: string;
+    id?: string;
+    stellarTransactionId?: string;
+    externalTransactionId?: string;
+    lang?: string;
+  }) {
+    if (!id && !stellarTransactionId && !externalTransactionId) {
+      throw new MissingTransactionIdError();
+    }
+
+    const toml = await this.getInfo();
+    const transferServerEndpoint = toml.transferServerSep24;
+
+    let qs: { [name: string]: string } = {};
+
+    if (id) {
+      qs = { id };
+    } else if (stellarTransactionId) {
+      qs = { stellar_transaction_id: stellarTransactionId };
+    } else if (externalTransactionId) {
+      qs = { external_transaction_id: externalTransactionId };
+    }
+
+    if (lang) {
+      qs = { lang, ...qs };
+    }
+
+    try {
+      // TODO - use httpClient
+      const resp = await axios.get(`${transferServerEndpoint}/transaction?${queryString.stringify(qs)}`, {
+        headers: {
+          Authorization: `Bearer ${authToken}`,
+        },
+      });
+  
+      const transaction = resp.data?.transaction;
+
+      if(!transaction || Object.keys(transaction).length === 0) {
+        throw new InvalidTransactionResponseError(transaction);
+      }
+
+      return _normalizeTransaction(transaction);
     } catch (e) {
       throw new ServerRequestFailedError(e);
     }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,6 +1,8 @@
 import StellarSdk, { Keypair } from "stellar-sdk";
 
 import sdk from "../src";
+import { ServerRequestFailedError } from "../src/walletSdk/exception";
+
 const { walletSdk } = sdk;
 
 describe("Wallet", () => {
@@ -29,20 +31,22 @@ describe("Anchor", () => {
   });
   it("should give TOML info", async () => {
     const resp = await anchor.getInfo();
+
     expect(resp.webAuthEndpoint).toBe("https://testanchor.stellar.org/auth");
     expect(resp.currencies.length).toBe(2);
   });
 
   it("should be able to authenticate", async () => {
     const auth = await anchor.auth();
-
     const token = await auth.authenticate(accountKp);
+
     expect(token).toBeTruthy();
     expect(typeof token).toBe("string");
   });
 
   it("should get anchor services info", async () => {
     const serviceInfo = await anchor.getServicesInfo();
+
     expect(serviceInfo.deposit).toBeTruthy();
     expect(serviceInfo.withdraw).toBeTruthy();
   });
@@ -57,9 +61,11 @@ describe("Anchor", () => {
     const resp = await anchor
       .interactive()
       .deposit(accountKp.publicKey(), assetCode, token);
+
     expect(resp.url).toBeTruthy();
     expect(resp.id).toBeTruthy();
   });
+
   it("should give interactive withdraw url", async () => {
     const auth = await anchor.auth();
     const token = await auth.authenticate(accountKp);
@@ -67,7 +73,40 @@ describe("Anchor", () => {
     const resp = await anchor
       .interactive()
       .withdraw(accountKp.publicKey(), assetCode, token);
+
     expect(resp.url).toBeTruthy();
     expect(resp.id).toBeTruthy();
+  });
+
+  it("should fetch existing transaction by id", async () => {
+    const auth = await anchor.auth();
+    const accountKp = Keypair.fromSecret(
+      "SDXC3OHSJZEQIXKEWFDNEZEQ7SW5DWBPW7RKUWI36ILY3QZZ6VER7TXV"
+    );
+    const token = await auth.authenticate(accountKp);
+    const transaction = await anchor
+      .getTransactionBy({ authToken: token, id: "da8575e9-edc6-4f99-98cf-2b302f203dd8" });
+  
+    const { id, kind, amount_in, amount_out, amount_fee } = transaction;
+
+    expect(transaction).toBeTruthy();
+    expect(id === "da8575e9-edc6-4f99-98cf-2b302f203dd8").toBeTruthy;
+    expect(kind === "deposit").toBeTruthy;
+    expect(amount_in === "100.15").toBeTruthy;
+    expect(amount_out === "99.15").toBeTruthy;
+    expect(amount_fee === "1.00").toBeTruthy;
+  });
+
+  it("should error fetching non-existing transaction by id", async () => {
+    const auth = await anchor.auth();
+    const accountKp = Keypair.fromSecret(
+      "SDXC3OHSJZEQIXKEWFDNEZEQ7SW5DWBPW7RKUWI36ILY3QZZ6VER7TXV"
+    );
+    const token = await auth.authenticate(accountKp);
+
+    await expect(async () => { 
+      const nonExistingTransactionId = "da8575e9-edc6-4f99-98cf-2b302f203cc7";
+      await anchor.getTransactionBy({ authToken: token, id: nonExistingTransactionId });
+    }).rejects.toThrowError(ServerRequestFailedError)
   });
 });
