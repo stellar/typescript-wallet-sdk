e5f7fda747070bdbddfeca472d50d9d1af89add0
Alec Charbonneau <aleccharb21@gmail.com>
2023-06-13 12:16:09 +0100

    cleanup

--- src/walletSdk/horizon/Stellar.ts
+++ src/walletSdk/horizon/Stellar.ts
@@ -1,5 +1,3 @@
-// TODO - https://stellarorg.atlassian.net/browse/WAL-814?atlOrigin=eyJpIjoiMWY5MjBkZTE4MTg3NDA3N2E0MjYwMmQ2ZmRhOGFiODUiLCJwIjoiaiJ9
-
 import { AccountService } from "./AccountService";
 
 // Do not create this object directly, use the Wallet class.
