387e2f988d5d91f79734e0f27eceaf8662427fb2
Alec Charbonneau <aleccharb21@gmail.com>
2023-06-14 11:04:04 +0100

    return type

--- src/walletSdk/horizon/AccountService.ts
+++ src/walletSdk/horizon/AccountService.ts
@@ -12,7 +12,7 @@ export class AccountService {
     this.network = cfg.stellar.network;
   }
 
-  createKeypair() {
+  createKeypair(): SigningKeypair {
     return new SigningKeypair(Keypair.random());
   }
 }
