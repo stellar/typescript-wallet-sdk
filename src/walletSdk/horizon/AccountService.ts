44202897d86f2a6b9f27384178bfdae68b9d4646
Alec Charbonneau <aleccharb21@gmail.com>
2023-06-14 11:07:39 +0100

    Config type

--- src/walletSdk/horizon/AccountService.ts
+++ src/walletSdk/horizon/AccountService.ts
@@ -1,13 +1,14 @@
 import { Keypair } from "stellar-sdk";
 
 import { SigningKeypair } from "./Account";
+import { Config } from "walletSdk";
 
 // Do not create this object directly, use the Wallet class.
 export class AccountService {
   private server;
   private network;
 
-  constructor(cfg) {
+  constructor(cfg: Config) {
     this.server = cfg.stellar.server;
     this.network = cfg.stellar.network;
   }
--- src/walletSdk/horizon/Stellar.ts
+++ src/walletSdk/horizon/Stellar.ts
@@ -1,9 +1,10 @@
 import { AccountService } from "./AccountService";
+import { Config } from "walletSdk";
 
 // Do not create this object directly, use the Wallet class.
 export class Stellar {
-  private cfg;
-  constructor(cfg) {
+  private cfg: Config;
+  constructor(cfg: Config) {
     this.cfg = cfg;
   }
 
