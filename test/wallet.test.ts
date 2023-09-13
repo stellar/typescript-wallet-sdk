7dcd4de71dbb24f5720433599901a74e71094d2e
Alec Charbonneau <aleccharb21@gmail.com>
2023-09-12 19:16:08 -0600

    add default domain signer and default client_domain (#53)
    * add default domainsigner
    
    * allow custom client
    
    * remove gitignore
    
    * add default client domain
    
    * cleanup
    
    * use mandatary headers object
    
    * add js doc comment

--- test/wallet.test.ts
+++ test/wallet.test.ts
@@ -18,6 +18,7 @@ import {
 } from "../src/walletSdk/Auth/WalletSigner";
 import { SigningKeypair } from "../src/walletSdk/Horizon/Account";
 import { Sep24 } from "../src/walletSdk/Anchor/Sep24";
+import { DomainSigner } from "../src/walletSdk/Auth/WalletSigner";
 
 import { TransactionsResponse } from "../test/fixtures/TransactionsResponse";
 
@@ -1801,6 +1802,29 @@ describe("Anchor", () => {
       // stops watching
       stop();
     });
+  });
+});
+
+describe("DomainSigner", () => {
+  it("should work", async () => {
+    jest.spyOn(DefaultClient, "post").mockResolvedValue({
+      data: {
+        transaction:
+          "AAAAAgAAAADVJRbxdB+qXZjUMLcrL/VVoS6megW1ReSxIO33pvO61AAAB9AAAAAAAAAAAQAAAAEAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAEAAAAAAAAACwAAAAAAAAACAAAAAAAAAAA=",
+      },
+    });
+    const signer = new DomainSigner("example url", {
+      SampleHeader: "sample-header",
+    });
+
+    const txn = await signer.signWithDomainAccount({
+      transactionXDR: "test-xdr",
+      networkPassphrase: "Test SDF Network ; September 2015",
+      accountKp: SigningKeypair.fromSecret(
+        "SBYAW5H46NNDGCECWMWWM32DE4DPNN3RHVMNTR3BXXZX2DJF6LZWBMWZ",
+      ),
+    });
+    expect(txn).toBeTruthy();
   });
 });
 
