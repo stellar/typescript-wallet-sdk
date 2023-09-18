50f06c2c0a09f0b056c8ed86f5336524b12113ab
Alec Charbonneau <aleccharb21@gmail.com>
2023-09-18 17:59:27 -0600

    cleanup

--- examples/sep24/test.ts
+++
@@ -1,7 +0,0 @@
-import { walletSdk } from "@stellar/typescript-wallet-sdk";
-import { Memo, MemoText } from "stellar-sdk";
-import axios from "axios";
-import readline from "readline";
-import open from "open";
-
-console.log({ walletSdk }); // ALEC TODO - remove
