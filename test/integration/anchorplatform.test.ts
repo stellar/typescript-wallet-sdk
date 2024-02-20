25e80ad97fb3e5f3ba8ed36642905200248d27c5
Alec Charbonneau <aleccharb21@gmail.com>
2024-02-20 15:33:16 -0800

    try

--- test/integration/anchorplatform.test.ts
+++ test/integration/anchorplatform.test.ts
@@ -23,7 +23,7 @@ describe("ALEC TODO", () => {
     anchor = wallet.anchor({ homeDomain: anchorUrl });
     accountKp = stellar.account().createKeypair();
     await stellar.fundTestnetAccount(accountKp.publicKey);
-  }, 300000);
+  }, 600000);
 
   it("auth should work", async () => {
     const auth = await anchor.sep10();
@@ -76,14 +76,15 @@ describe("ALEC TODO", () => {
 
 const isServerReady = async () => {
   let ready = false;
-  for (let fails = 0; fails < 20; fails++) {
+  for (let fails = 0; fails < 60; fails++) {
     try {
       const resp = await axios.get(`${anchorUrl}/.well-known/stellar.toml`);
       console.log({ resp }); // ALEC TODO - remove
       ready = true;
       break;
     } catch (e) {
-      await new Promise((resolve) => setTimeout(resolve, 15000));
+      console.log({ e }); // ALEC TODO - remove
+      await new Promise((resolve) => setTimeout(resolve, 10000));
     }
   }
   return ready;
