39408125f5a162a1601b037b4666bc5a8f500a75
Alec Charbonneau <aleccharb21@gmail.com>
2024-03-11 12:39:49 -0700

    create workspace (#101)
    * create workspace
    
    * try
    
    * try
    
    * try
    
    * try
    
    * cleanup
    
    * update anchor platform docker run
    
    * fix
    
    * fix
    
    * upgrade babel/traverse
    
    * upgrade browserify-sign
    
    * add keypair package in a second workspace (#102)
    
    * add km package
    
    * fix tests
    
    * cleanup

---
+++ @stellar/typescript-wallet-sdk-km/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = (env = { NODE: false }) => ({
+  mode: "development",
+  entry: "./src/index.ts",
+  devtool: "source-map",
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".json", ".ts"],
+    fallback: {
+      crypto: require.resolve("crypto-browserify"),
+      http: require.resolve("stream-http"),
+      https: require.resolve("https-browserify"),
+      stream: require.resolve("stream-browserify"),
+      url: require.resolve("url"),
+      util: require.resolve("util"),
+    },
+  },
+  output: {
+    library: "WalletSdkKm",
+    libraryTarget: "umd",
+    globalObject: "this",
+    filename: `bundle${!env.NODE ? "_browser" : ""}.js`,
+    path: path.resolve(__dirname, "lib"),
+  },
+  target: env.NODE ? "node" : "web",
+});
