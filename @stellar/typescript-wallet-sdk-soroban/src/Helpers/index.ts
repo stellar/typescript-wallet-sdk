25e455c2eafaa9ad96ff3c54fe29f801c5ff9002
Cássio Marcos Goulart <cassiomgoulart@gmail.com>
2024-04-24 14:57:29 -0300

    rename old tokenInvocation => getTokenInvocationArgs

--- @stellar/typescript-wallet-sdk-soroban/src/Helpers/index.ts
+++ @stellar/typescript-wallet-sdk-soroban/src/Helpers/index.ts
@@ -1,5 +1,5 @@
 export * from "./formatTokenAmount";
 export * from "./getInvocationDetails";
+export * from "./getTokenInvocationArgs";
 export * from "./parseTokenAmount";
 export * from "./scValByType";
-export * from "./tokenInvocation";
